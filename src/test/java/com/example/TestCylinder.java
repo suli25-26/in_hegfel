07690b5de12535b71a234835e4e13ef71567814d
Erős István <eros@valahol.dev>
2026-04-14 15:00:38 +0200

    Teszt elkezdve

---
+++ src/test/java/com/example/TestCylinder.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestCylinder {
+    @Test public void testCalcSurfaceData1() {
+        Assert.assertTrue(true);
+    }
+}
